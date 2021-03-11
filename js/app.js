f22266a34aadaa4ca7918a80178f2e70a678c19b
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-03-11 15:56:01 +0100

    Added javascript to the testimonials section

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// TESTIMONIAL SLIDER
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slides = document.querySelectorAll('.testimonial__box--col');
+
+
+
+let index = 0;
+display(index);
+function display(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'block';
+}
+
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display(index);
+}
+
+function nextSlide() {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
